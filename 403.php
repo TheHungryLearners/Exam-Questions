eaef59267abdcad1bb366695dd81ef4699906261
John <john.17.dc@gmail.com>
2014-11-25 20:57:20 +0530

    changes made to error page

--- 403.php
+++ 403.php
@@ -1,12 +1,16 @@
-<!doctype html>
-<html class="no-js" lang="en">
-<?php include_once('header.php'); ?>
+<?php 
+
+    // below line sets the title of the page, use this variable before including header.php on any page
+    $page_title = "Error!";
+    include_once('header.php'); 
+?>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/foundation.css" />
     <script src="js/vendor/modernizr.js"></script>
     <style>
+    /*Put all this code in the my_css file, check the one already there, add only the ones which differ*/
 		body, .panel
 		{
 			background: #222222;
@@ -28,12 +32,14 @@
     }
     </style>
   </head>
+  <!-- use body tags for sure -->
     <div class="row">
       <h3><b>Error: Forbidden Access</b></h3> 
     </div>
 
 <?php 
+  // make sure the footer stays at the bottom of the page, else it looks weird
   include_once('footer.php');
-  header('Refresh: 3; url = index.php'); 
+//  header('Refresh: 3; url = index.php'); 
 ?>
 
